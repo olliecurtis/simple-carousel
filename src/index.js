a531a4d60888625cb3641e9cdf151a2231eeec9a
Ollie Curtis <8831547+olliecurtis@users.noreply.github.com>
2025-04-15 13:20:55 +0100

    Spring clean the deps

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import 'bpk-stylesheets/base';
-import 'bpk-stylesheets/base.css';
+import '@skyscanner/backpack-web/bpk-stylesheets/base';
+import '@skyscanner/backpack-web/bpk-stylesheets/base.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
